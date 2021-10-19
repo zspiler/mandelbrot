8b170690e19799cadd0c6321a4f09b6b7749ff6b
zanspiler <zanspiler1@gmail.com>
2021-10-19 11:51:51 +0200

    refactor

--- mandelbrot.c
+++ mandelbrot.c
@@ -9,17 +9,15 @@
        
 #include <unistd.h>
 
+#define NTHREADS 8
+#define WIDTH 1750
+#define HEIGHT 1000
 
-const int width = 1750;
-const int height = 1000;
-const int R = 2 * 2; // escape radius
-
-const double focus_x = -0.743291725;
-const double focus_y = -0.131234623;
-
-const int max_iterations = 1000;
-const double zoom = 0.001;
-
+#define R 2 * 2 // escape radius
+#define FOCUS_X -0.743291725 // default -0.75
+#define FOCUS_Y -0.131234623 // default 0
+#define MAX_ITER 1000
+#define ZOOM 0.001 // default 1
 
 struct Color { 
     int r; 
@@ -36,38 +34,38 @@ struct Color map_to_color(int i, int mode) {
     switch (mode){
         default:
         case 0: // b&w
-            r = 255.0 / (max_iterations) * i;
+            r = 255.0 / (MAX_ITER) * i;
             g = r;
             b = r;
             break;
         case 1: // red
-            if (i < max_iterations / 3) {
-                r = 255.0 / (max_iterations / 3) * i;
+            if (i < MAX_ITER / 3) {
+                r = 255.0 / (MAX_ITER / 3) * i;
             }
-            else if (i < 2/3 * max_iterations) {
+            else if (i < 2/3 * MAX_ITER) {
                 r = 255;
-                g = 255.0 / (2/3 * max_iterations - max_iterations / 3) * (i - max_iterations / 3);
+                g = 255.0 / (2/3 * MAX_ITER - MAX_ITER / 3) * (i - MAX_ITER / 3);
             }
             else {
                 r = 255;
                 g = 255;
-                b = 255.0 / (max_iterations - (2/3 * max_iterations)) * (i - (2/3 * max_iterations));
+                b = 255.0 / (MAX_ITER - (2/3 * MAX_ITER)) * (i - (2/3 * MAX_ITER));
             }
             break;
     
         case 2: // purple
-            if (i < max_iterations / 3) {
-                b = 255.0 / (max_iterations / 3) * i;
-                r = 255.0 / (max_iterations / 3) * i;
+            if (i < MAX_ITER / 3) {
+                b = 255.0 / (MAX_ITER / 3) * i;
+                r = 255.0 / (MAX_ITER / 3) * i;
             }
-            else if (i < 2/3 * max_iterations) {
+            else if (i < 2/3 * MAX_ITER) {
                 b = 255;
-                g = 255.0 / (2/3 * max_iterations - max_iterations / 3) * (i - max_iterations / 3);
+                g = 255.0 / (2/3 * MAX_ITER - MAX_ITER / 3) * (i - MAX_ITER / 3);
             }
             else {
                 b = 255;
                 g = 255;
-                r = 255.0 / (max_iterations - (2/3 * max_iterations)) * (i - (2/3 * max_iterations));
+                r = 255.0 / (MAX_ITER - (2/3 * MAX_ITER)) * (i - (2/3 * MAX_ITER));
             }
             break;
     }
@@ -84,83 +82,37 @@ struct Color map_to_color(int i, int mode) {
     spiral          tunnel                  tunel             
 */
 
-
-// default focus (-0.75, 0), zoom 1
-
-void draw(char *filename) { 
-
-    cairo_surface_t *surface = cairo_image_surface_create (CAIRO_FORMAT_RGB24, width, height);
-    cairo_t *cr = cairo_create (surface);
-
-    const double from_re = focus_x - (3.5 * zoom)/2; 
-    const double to_re = focus_x + (3.5 * zoom)/2; 
-    const double from_im = focus_y - (2 * zoom)/2; 
-    const double to_im = focus_y + (2 * zoom)/2; 
-
-    for(int y = 0; y <= height; y++) {
-        for(int x = 0; x <= width; x++) {
-            double x0 = (((double)x/(double)width) * (to_re - (from_re)) + from_re); 
-            double y0 = (((double)y/(double)height) * (to_im - from_im) + to_im); 
-
-            double complex z = 0.0 + 0.0 * I;  
-            int i = 0;
-            while (creal(z) + cimag(z) < R && i < max_iterations) {
-                double complex c = x0 + y0 * I;             
-                z = z * z + c;
-                i++;
-            }
-
-            if (i < max_iterations) {
-                struct Color col = map_to_color(i, 1);
-                // plot
-                cairo_set_source_rgb (cr, col.r/255.0, col.g/255.0, col.b/255.0);
-                cairo_rectangle (cr, x, y, 1.0, 1.0);
-                cairo_fill (cr);
-            }
-        }
-        
-        printf("\rrow %d/%d", y, height);
-        fflush(stdout);
-    }
-
-    cairo_destroy (cr);
-    cairo_surface_write_to_png (surface, strlen(filename) > 0 ? filename : "image.png");
-    cairo_surface_destroy (surface);
-}
-
-
 struct ThreadArgs {
     int start_y;
     int end_y;
-    double zoom;
     cairo_t *cr;
-    struct Color (*image)[height][width];
+    struct Color (*image)[HEIGHT][WIDTH];
 };
 
 
-void *draw_multithread(void *vargp) {    
+void *draw(void *vargp) {    
     struct ThreadArgs* args = ((struct ThreadArgs*)vargp);
 
-    const double fromR = focus_x - (3.5 * zoom)/2; 
-    const double toR = focus_x + (3.5 * zoom)/2; 
-    const double fromI = focus_y - (2 * zoom)/2; 
-    const double toI = focus_y + (2 * zoom)/2; 
+    const double fromR = FOCUS_X - (3.5 * ZOOM)/2; 
+    const double toR = FOCUS_X + (3.5 * ZOOM)/2; 
+    const double fromI = FOCUS_Y - (2 * ZOOM)/2; 
+    const double toI = FOCUS_Y + (2 * ZOOM)/2; 
 
     for(int y = args->start_y; y < args->end_y; y++) {
-        for(int x = 0; x < width; x++) {
-            double x0 = (((double)x/(double)width) * (toR - (fromR)) + fromR); 
-            double y0 = (((double)y/(double)height) * (toI - fromI) + fromI); 
+        for(int x = 0; x < WIDTH; x++) {
+            double x0 = (((double)x/(double)WIDTH) * (toR - (fromR)) + fromR); 
+            double y0 = (((double)y/(double)HEIGHT) * (toI - fromI) + fromI); 
 
             double complex z = 0.0 + 0.0 * I;  
             int i = 0;
 
-            while (creal(z) + cimag(z) < R && i < max_iterations) {
+            while (creal(z) + cimag(z) < R && i < MAX_ITER) {
                 double complex c = x0 + y0 * I;             
                 z = z * z + c;
                 i++;
             }
             
-            if (i < max_iterations) {
+            if (i < MAX_ITER) {
                 struct Color col = map_to_color(i, 1);
                 (*(args->image))[y][x] = col;
             }
@@ -176,64 +128,50 @@ int main() {
     struct timeval start_time, end_time;
     gettimeofday(&start_time, NULL);
 
-
-    // draw(0.0000000000001, "");
-
-    // int i = 0;
-    // for (double zoom = 1; zoom >= 0.0001; zoom *= 0.9) { // 0.99
-    //     char file[100];
-    //     sprintf(file, "./sequence/%d.png", i);    
-    //     i += 1;
-    //     draw(-0.34853774148008254, -0.6065922085831237, zoom, file);
-    // }
-    
-
-    // Multithread
-    cairo_surface_t *surface = cairo_image_surface_create (CAIRO_FORMAT_RGB24, width, height);
+    cairo_surface_t *surface = cairo_image_surface_create (CAIRO_FORMAT_RGB24, WIDTH, HEIGHT);
     cairo_t *cr = cairo_create (surface);
 
-    const int num_of_threads = 4;
-    pthread_t threads[num_of_threads];
+    pthread_t threads[NTHREADS];
 
-    static struct Color image[height][width];
+    static struct Color image[HEIGHT][WIDTH];
 
     struct ThreadArgs *args;
-    for (int i = 0; i < num_of_threads; i++) {    
+    for (int i = 0; i < NTHREADS; i++) {    
         args = (struct ThreadArgs *) malloc(sizeof(struct ThreadArgs));
-        args->start_y = i * height/num_of_threads;
-        args->end_y = i * height/num_of_threads + height/num_of_threads;
+        args->start_y = i * HEIGHT/NTHREADS;
+        args->end_y = i * HEIGHT/NTHREADS + HEIGHT/NTHREADS;
         args->cr = cr;
         args->image = &image;
     
-        pthread_create(&threads[i], NULL, draw_multithread, args);
+        pthread_create(&threads[i], NULL, draw, args);
+        
     }
     free(args);
 
-    for (int i = 0; i < num_of_threads; i++) {
+    for (int i = 0; i < NTHREADS; i++) {
         pthread_join(threads[i], NULL);
     }
     
     // plot 
-    for(int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            struct Color col = image[y][x]; // To je sam en args retard
+    for(int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            struct Color col = image[y][x]; 
             cairo_set_source_rgb (cr, col.r/255.0, col.g/255.0, col.b/255.0);
             cairo_rectangle (cr, x, y, 1.0, 1.0);
             cairo_fill (cr);
         }
     }
 
+    // create image
     cairo_destroy (cr);
     cairo_surface_write_to_png (surface, "image.png");
     cairo_surface_destroy (surface);
 
-
-    
    
     gettimeofday(&end_time, NULL);
     double exec_time = end_time.tv_sec + end_time.tv_usec / 1e6 -
                         start_time.tv_sec - start_time.tv_usec / 1e6; 
-    printf("\ntime: %.2f\n", exec_time);
+    printf("time: %.2fs\n", exec_time);
 
     return 0;
 }
